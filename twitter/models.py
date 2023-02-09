e99eb52210ececb2c9487927c1974e0ec9954fc0
zaripov4 <zaripovulugbek45@gmail.com>
2023-02-09 20:10:22 +0500

    user model added

--- twitter/models.py
+++ twitter/models.py
@@ -1,2 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    dob = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.username()
+
+    class Meta:
+        db_table = 'users'
 
